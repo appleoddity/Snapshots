1a78a36e96aea88607cba4c28aec7f1b0bc1c7a0
Appleoddity <29755504+appleoddity@users.noreply.github.com>
2021-08-12 19:55:19 -0400

    Several bug fixes and impovements
    Fixes:
    -- Calculate proper snapshot size when changed block count is over MaxResults (10,000) by recognizing NextToken in the response.
    -- Calculate proper snapshot size of the first snapshot on a volume by using ebs.list_snapshot_blocks instead of arbitrarily using the EBS volume size.
    -- Calculate proper snapshot size by using the reported snapshot blocksize rather than a hardcoded value that may change.
    -- Fixes to the date/time code to account for datetimes that are missing milliseconds, as seen in some snapshot results.
    
    Code optimized and refactored for simplicity and clarity.
    
    This new script can take significantly longer to run because it accurately calculates the total storage used by every snapshot which sometimes requires more than one API request.

--- snapshotsSize.py
+++ snapshotsSize.py
@@ -12,29 +12,32 @@ df = pd.DataFrame.from_dict(snapshots['Snapshots'])
 df.sort_values(by=['OwnerId', "VolumeId", "StartTime"], inplace = True)
 
 # per volumeid lineage, get for each one changed blocks
-i = 0
-l = len(df.index)
-first = True
-blockSize = 524288
+v_prev = ''
+sid_prev = ''
 for index, row in df.iterrows():
-    if i == l:
-        break
-    date = str(row['StartTime']).split('.',1)[0].split(' ')[0]
-    time = str(row['StartTime']).split('.',1)[0].split(' ')[1]
-    if first:
-        v_prev = row['VolumeId']
-        sid_prev = row['SnapshotId']
-        print(date + ',' + time + ',' + v_prev + "," + 'snap-00000000000000000' + "," + sid_prev + "," + str(row['VolumeSize'] * 1024 * 1024 * 1024))
-        first = False
-        i = i + 1
-        continue
-    v = row['VolumeId']
-    sid = row['SnapshotId']
-    if v == v_prev:
-        changed = len(ebs.list_changed_blocks(FirstSnapshotId = sid_prev,SecondSnapshotId = sid)['ChangedBlocks'])
-        print(date + ',' + time + ',' + v + "," + sid_prev + "," + sid + "," + str(changed * blockSize))
+    #Break StartTime into individual date and time
+    date = str(row['StartTime']).split('+')[0].split('.')[0].split(' ')[0] #Strip milliseconds and offset
+    time = str(row['StartTime']).split('+')[0].split('.')[0].split(' ')[1] #Strip milliseconds and offset
+
+    if row['VolumeId'] == v_prev: # If current volume ID matches previous volume ID we are working on the same volume
+        # Get changed block size between this snapshot and the previous snapshot for the same volume
+        changed_blocks = ebs.list_changed_blocks(FirstSnapshotId = sid_prev, SecondSnapshotId = row['SnapshotId'], MaxResults = 10000)
+        block_size = changed_blocks['BlockSize'] # Let EBS tell us the blocksize of the snapshot
+        changed = len(changed_blocks['ChangedBlocks'])
+        while 'NextToken' in changed_blocks: # If NextToken is present than we have more block changes to get
+            changed_blocks = ebs.list_changed_blocks(FirstSnapshotId = sid_prev,SecondSnapshotId = row['SnapshotId'], MaxResults = 10000, NextToken = changed_blocks['NextToken'])
+            changed += len(changed_blocks['ChangedBlocks'])
     else:
-        print(date + ',' + time + ',' + v + "," + 'snap-00000000000000000' + "," + sid + "," + str(row['VolumeSize'] * 1024 * 1024 * 1024))
-    v_prev = v
-    sid_prev = sid
-    i = i + 1
+        sid_prev = 'snap-00000000000000000' # There is no previous snapshot SID for this volume
+        # Get total number of blocks in the first snapshot for this volume
+        changed_blocks = ebs.list_snapshot_blocks(SnapshotId=row['SnapshotId'], MaxResults=10000)
+        block_size = changed_blocks['BlockSize'] # Let EBS tell us the blocksize of the snapshot
+        changed = len(changed_blocks['Blocks'])
+        while 'NextToken' in changed_blocks: # If NextToken is present than we have more block changes to get
+            changed_blocks = ebs.list_snapshot_blocks(SnapshotId=row['SnapshotId'], MaxResults=10000, NextToken=changed_blocks['NextToken'])
+            changed += len(changed_blocks['Blocks'])
+    # Output the results
+    print(date + ',' + time + ',' + row['VolumeId'] + "," + sid_prev + "," + row['SnapshotId'] + "," + str(changed * block_size))
+    # Store the current volumeId and snapshotId for next iteration
+    v_prev = row['VolumeId']
+    sid_prev = row['SnapshotId']
